637cdd8eaf99e705fe95aaaa0179808a50193132
Sino <96530699+Sino1507@users.noreply.github.com>
2024-10-16 14:15:42 +0200

    Update ModuleEditor.lua

--- Libs/ModuleEditor.lua
+++ Libs/ModuleEditor.lua
@@ -3,7 +3,11 @@ ModuleEditor.__index = ModuleEditor
 
 local args = {...}
 if #args == 0 then error('[ModuleEditor] Insufficient arguments. (Expected atleast 1 Argument -> ModuleScript)') end 
-if args[1].ClassName ~= 'ModuleScript' and typeof(args[1]) ~= 'table' then error('[ModuleEditor] Incorrect arguments. (Expected atleast 1 Argument -> ModuleScript/Table)') end
+if typeof(args[1]) == 'userdata' and args[1].ClassName ~= 'ModuleScript' then
+    if typeof(args[1]) ~= 'table' then 
+        error('[ModuleEditor] Incorrect arguments. (Expected atleast 1 Argument -> ModuleScript/Table)') 
+    end
+end
 
 ModuleEditor.Lib = {}
 ModuleEditor.Result = {}
