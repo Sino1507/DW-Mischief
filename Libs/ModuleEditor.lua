4ab70c7bd5c77dd6c1d2e5a8cecc64f5ba111422
Sino <96530699+Sino1507@users.noreply.github.com>
2024-10-16 14:11:23 +0200

    Update ModuleEditor.lua

--- Libs/ModuleEditor.lua
+++ Libs/ModuleEditor.lua
@@ -2,13 +2,12 @@ local ModuleEditor = {}
 ModuleEditor.__index = ModuleEditor
 
 local args = {...}
-print(#args)
 if #args == 0 then error('[ModuleEditor] Insufficient arguments. (Expected atleast 1 Argument -> ModuleScript)') end 
+if args[1].ClassName ~= 'ModuleScript' and typeof(args[1]) ~= 'table' then error('[ModuleEditor] Incorrect arguments. (Expected atleast 1 Argument -> ModuleScript/Table)') end
 
 ModuleEditor.Lib = {}
 ModuleEditor.Result = {}
 ModuleEditor.Query = {}
 
-print(game:GetService('HttpService'):JSONEncode(args))
 return ModuleEditor
 
